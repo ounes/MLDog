02ec639cfa9e8ed88972577760dce424c014f7a5
ounes <ounes.chadli@gmail.com>
2018-03-16 18:07:17 +0100

    Optimize the algorithm for a precision of 99.9%

--- dog_ml.py
+++ dog_ml.py
@@ -6,7 +6,7 @@ from extract_features import extract_features_from_binary, get_model
 
 
 def chargement(sc, dir):
-	rdd_image = sc.binaryFiles(dir).cache()
+	rdd_image = sc.binaryFiles(dir, minPartitions=1).cache()
 	return rdd_image
 
 
@@ -15,7 +15,7 @@ def split_rdd(rdd):
 		Separate a rdd into two weighted rdds train(70%) and test(30%)
 		:param rdd
 	"""
-	SPLIT_WEIGHT = 0.5
+	SPLIT_WEIGHT = 0.7
 	(rdd_train, rdd_test) = rdd.randomSplit([SPLIT_WEIGHT, 1 - SPLIT_WEIGHT])
 	return rdd_train, rdd_test
 
@@ -30,16 +30,15 @@ def map_model(partition_data):
 
 
 def main():
-	sc = SparkContext(master="local[2]",appName="2typesDogs")
+	sc = SparkContext(appName="2typesDogs")
 	rdd_image = chargement(sc, sys.argv[1])
 
-	rdd_image = rdd_image.mapPartitions(map_model)
+	rdd_image = rdd_image.mapPartitions(map_model,preservesPartitioning=True)
 
 	rdd_train, rdd_test = split_rdd(rdd_image)
 
-
-	svm=SVMWithSGD.train(prepare_data(rdd_train))
-	rdd_test = rdd_test.map(lambda name_content: (name_content[0], svm.predict(name_content[1])))
+	svm = SVMWithSGD.train(prepare_data(rdd_train))
+	rdd_test = rdd_test.map(lambda name_content: (name_content[0], svm.predict(name_content[1]))).cache()
 	rdd_test = rdd_test.map(lambda name_predicted: (float(1) if 'yorkshire' in name_predicted[0] else float(0), name_predicted[1]))
 	nb_dogs_test = rdd_test.count()
 	nb_correct_dogs = rdd_test.filter(lambda reel_predicted: reel_predicted[0]==reel_predicted[1]).count()
@@ -49,7 +48,7 @@ def main():
 	print ("Total number of dogs :",nb_dogs_test)
 	print ("Accuracy :",accuracy)
 
-
+	input("Appuyez sur CTR+C")
 
 if __name__ == "__main__":
 	main()

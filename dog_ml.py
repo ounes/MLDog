ade8a526be23218799ec53c6a9a9e8f9e2cdcde1
Benoit Golberg <benoit.golberg@gmail.com>
2018-03-16 12:21:30 +0100

    split des rdd

--- dog_ml.py
+++ dog_ml.py
@@ -3,14 +3,19 @@ from pyspark import SparkContext
 
 
 def chargement(sc,dir):
-	rdd_image=sc.binaryFiles(dir)
+	rdd_image=sc.binaryFiles(dir).cache()
 	return rdd_image
 	
+def split_rdd(rdd):
+	(rdd_train, rdd_test) = rdd.randomSplit([0.7,0.3])
+	return rdd_train, rdd_test
 	
 def main():
 	sc= SparkContext(master="local[2]",appName="2typesDogs")
 	rdd_image=chargement(sc,sys.argv[1])
-	print rdd_image.first()
+	
+	rdd_train, rdd_test = split_rdd(rdd_image)
+
 
 if __name__ == "__main__":
 	main()
